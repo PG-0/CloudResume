1e9d93c341739c2f8da92ed94aafd87b35fd325e
pgg6 <pgg6@njit.edu>
2023-02-10 16:01:33 -0500

    Python Test Scripts

---
+++ Python_Scripts/test_API-DDB-GetItem.py
@@ -0,0 +1,24 @@
+import requests
+# Requests is a HTTP library for python
+
+endpoint = "https://sw0sdet3ol.execute-api.us-east-1.amazonaws.com/retrieve-count"
+
+response = requests.get(endpoint)
+
+print(response)
+
+data = response.json()
+print("Number returned from DB " + data)
+
+status_code = response.status_code
+print("Status Code: ", status_code)
+
+
+# Sanity check to make sure that the endpoint works
+def test_can_call_endpoint():
+    response = requests.get(endpoint)
+    assert response.status_code == 200
+
+# Resources used: 
+# https://www.youtube.com/watch?v=7dgQRVqF1N0 (Thank you Pixegami)
+# Command to run pytest " python3 -m pytest -v -s" -> Prints results
