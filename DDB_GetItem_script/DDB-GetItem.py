e69f3ed89628dfb8bbb5cc6737b9339e78fe4628
Peter G <peterg8601@gmail.com>
2023-02-09 21:01:46 -0500

    Add files via upload

---
+++ DDB_GetItem_script/DDB-GetItem.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+import os
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Decimal):
+      return str(obj)
+    return json.JSONEncoder.default(self, obj)
+
+def lambda_handler(event, context):
+   
+    dynamodb = boto3.resource('dynamodb')
+    table_name = dynamodb.Table(os.environ['DDB_table'])
+   
+    response = table_name.get_item(
+      Key = {'visitorID':'visitor-x'},
+      ProjectionExpression="visit_count"
+    )
+    count = response['Item']['visit_count']
+    print(count)
+   
+    return {
+        'statusCode': 200,
+        'body': json.dumps(count, cls=DecimalEncoder)
+    }
